20eacf8bacece4b49553704545d9c98e274b62c9
Name <Email@gmail.com>
2025-05-22 22:24:47 +0800

    Static initializers

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,9 @@ import 'package:chat_apps/widgets/counter_stateful_demo.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await AuthService.init();
   runApp(Provider(create: (BuildContext context) => AuthService(),
   child: const ChatApp(),
   ));
